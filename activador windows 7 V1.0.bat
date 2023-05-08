0bababeabac5877b2f9fba00e775946458a31468
shaggy_fino <103294216+finoshaggy@users.noreply.github.com>
2023-05-07 21:49:06 -0400

    Add files via upload

---
+++ activador windows 7 V1.0.bat
@@ -0,0 +1,46 @@
+@echo off
+
+title activar windows 7
+echo:
+echo:______________________________________________________________________
+echo: activador para windows 7 en todas sus versiones
+echo: 
+echo: este activador solo funciona por unos dias y tienes que volver a usarlo
+echo: 
+echo: preciona cualquier tecla para activar su windows 7
+echo:______________________________________________________________________
+echo:
+
+pause
+cls
+color 9
+echo:
+echo:______________________________________________________________________
+echo: "ACTIVANDO WINDOWS 7"
+echo:______________________________________________________________________
+echo:
+slmgr -rearm
+cls
+color A
+echo:
+echo:______________________________________________________________________
+echo: "Su windows 7 a sido activado con exito"
+echo: 
+echo: preciona cualquier tecla para continuar
+echo: 
+echo: "Version del activador: V1.0"
+echo:______________________________________________________________________
+echo:
+pause
+cls
+echo:
+echo:______________________________________________________________________
+echo: "Si tiene algun problema con el activador contactar a Shaggydreansificado#0460"
+echo: 
+echo: Para aplicar los canbios reiniciar su PC 
+echo:
+echo: Futuras actualizaciones se agregaran mas activadores para windows 8,8.1,10 y 11
+echo: 
+echo: preciona cualquier tecla para cerra este cmd
+echo:______________________________________________________________________
+pause
